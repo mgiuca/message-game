1f0c89a6dae3a7239b9988818b3075f6ccda39bb
Matt Giuca <matt.giuca@gmail.com>
2026-04-19 09:47:44 +1000

    Image decoder: Implemented the decoder UI.
    Can now drag slider to adjust image.

--- scenes/levels/image_decoder.gd
+++ scenes/levels/image_decoder.gd
@@ -3,19 +3,42 @@ extends Level
 const IMAGE_TOTAL_PIXELS : int = 1679
 var image_width : int = 1
 
+@onready var source_image : Image = load('res://data/image.png')
+
 @onready var image_rect : TextureRect = %ImageRect
 @onready var lbl_size : Label = %LblSize
+
+# Must be 1 byte per pixel for the below algorithm to work.
+const IMAGE_FORMAT = Image.FORMAT_L8
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   super._ready()
+
   update_display()
 
 func update_display() -> void:
-  var image_height := roundi(float(IMAGE_TOTAL_PIXELS) / float(image_width))
+  var image_height : int = roundi(float(IMAGE_TOTAL_PIXELS) / float(image_width))
   lbl_size.text = '%dx%d' % [image_width, image_height]
 
-  # TODO: Redraw the image with the given width and height.
+  # Redraw the image with the given width and height. Just copy the pixels from
+  # src to dst, ignoring height.
+  assert(source_image.get_format() == IMAGE_FORMAT)
+  var src_data := source_image.get_data()
+  var src_size := source_image.get_data_size()
+  var data : PackedByteArray
+  data.resize(image_width * image_height)
+  for y in image_height:
+    for x in image_width:
+      var idx := y * image_width + x
+      var value : int = 0
+      if idx < src_size:
+        value = src_data[idx]
+      data[idx] = value
+  var rearranged_image := Image.create_from_data(image_width, image_height, false,
+                                                 IMAGE_FORMAT, data)
+  var image_tex := ImageTexture.create_from_image(rearranged_image)
+  image_rect.texture = image_tex
 
 func _on_sld_width_value_changed(value: float) -> void:
   # TODO: Make this scale evenly biased to width and height (i.e. the left end
