ecdc513bf92123a9c0049ddf26907107b1975db7
Matt Giuca <matt.giuca@gmail.com>
2026-04-19 09:21:07 +1000

    Make slider change size label.

--- scenes/levels/image_decoder.gd
+++ scenes/levels/image_decoder.gd
@@ -1,6 +1,24 @@
 extends Level
 
+const IMAGE_TOTAL_PIXELS : int = 1679
+var image_width : int = 1
+
+@onready var image_rect : TextureRect = %ImageRect
+@onready var lbl_size : Label = %LblSize
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   super._ready()
+  update_display()
+
+func update_display() -> void:
+  var image_height := roundi(float(IMAGE_TOTAL_PIXELS) / float(image_width))
+  lbl_size.text = '%dx%d' % [image_width, image_height]
+
+  # TODO: Redraw the image with the given width and height.
+
+func _on_sld_width_value_changed(value: float) -> void:
+  # TODO: Make this scale evenly biased to width and height (i.e. the left end
+  # affects width the same as the right end affects height).
+  image_width = roundi(value)
+  update_display()
