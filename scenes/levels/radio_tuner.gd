07f81605643ec2814fec79b0abf6d232abd2436b
Matt Giuca <matt.giuca@gmail.com>
2026-04-19 21:35:55 +1000

    RadioTuner: Restarted, copying from PulseDetector.
    So many similarities, and not enough time to abstract.

--- scenes/levels/radio_tuner.gd
+++ scenes/levels/radio_tuner.gd
@@ -1,27 +1,28 @@
 extends Level
 
-const SIGNAL_FREQUENCY : float = 673
-
 # Percent tolerance for getting the correct answer.
 const VERIFY_TOLERANCE_PCT : float = 1.15
-
-var source_image : Image = load('res://data/image.png')
-var signal_audio_stream : AudioStreamWAV
-var audio_stream : AudioStreamWAV
 
 var playing : bool = false:
   set(value):
     playing = value
     update_play_button_text()
+
+var source_image : Image = load('res://data/image.png')
+var signal_audio_stream : AudioStreamWAV
+var audio_stream : AudioStreamWAV
+
 var playhead_time : float:
   set(value):
     playhead_time = value
     if playhead:
       update_playhead()
 
-@onready var audio_stream_player : AudioStreamPlayer = $AudioStreamPlayer
 @onready var btn_play_stop : Button = %BtnPlayStop
+@onready var chk_only_visible : CheckBox = %ChkOnlyVisible
 @onready var waveform : Waveform = %Waveform
+@onready var audio_stream_player : AudioStreamPlayer = $AudioStreamPlayer
+
 @onready var playhead : VLine = %Waveform/PlayHead
 
 var filter_frequency : float = 50:
@@ -31,18 +32,34 @@ var filter_frequency : float = 50:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   super._ready()
+
   signal_audio_stream = PulseGenerator.generate_audio_from_image(source_image)
+  audio_stream_player.stream = audio_stream
+  waveform.audio_stream = audio_stream
+
+  update_playhead()
+
   update_play_button_text()
 
   update_filter_frequency()
 
 func _process(_delta: float) -> void:
   if audio_stream_player.playing and not audio_stream_player.stream_paused:
+    if chk_only_visible.button_pressed:
+      var t := audio_stream_player.get_playback_position()
+      if t < waveform.start_time or t > waveform.end_time:
+        audio_stream_player.seek(waveform.start_time)
+
     # Invalid when not playing.
     playhead_time = audio_stream_player.get_playback_position()
 
 func update_playhead() -> void:
   playhead.position.x = waveform.t_to_x(playhead_time)
+
+func update_filter_frequency() -> void:
+  audio_stream = signal_audio_stream # TODO
+  audio_stream_player.stream = audio_stream
+  waveform.audio_stream = audio_stream
 
 func seek(time: float) -> void:
   if audio_stream_player.playing and not audio_stream_player.stream_paused:
@@ -57,14 +74,6 @@ func update_play_button_text() -> void:
 func _on_btn_confirm_pressed() -> void:
   # TODO: Check
   LevelManager.switch_to_next_level_or_quit()
-
-func update_filter_frequency() -> void:
-  audio_stream = signal_audio_stream # TODO
-  audio_stream_player.stream = audio_stream
-  waveform.audio_stream = audio_stream
-
-func _on_sld_tune_freq_value_changed(value: float) -> void:
-  filter_frequency = value
 
 func play() -> void:
   audio_stream_player.play(playhead_time)
@@ -81,3 +90,15 @@ func _on_btn_play_stop_pressed() -> void:
     play()
   else:
     stop()
+
+func _on_audio_stream_player_finished() -> void:
+  playing = false
+
+func _on_waveform_start_drag(time: float) -> void:
+  seek(time)
+
+func _on_waveform_continue_drag(time: float) -> void:
+  seek(time)
+
+func _on_sld_tune_freq_value_changed(value: float) -> void:
+  filter_frequency = value
