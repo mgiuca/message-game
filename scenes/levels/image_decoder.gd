4fad40893b127973b37963bb8d24d4e1b666b23e
Matt Giuca <matt.giuca@gmail.com>
2026-04-19 23:49:34 +1000

    ImageDecoder: Don't hard-code sizes.

--- scenes/levels/image_decoder.gd
+++ scenes/levels/image_decoder.gd
@@ -1,8 +1,5 @@
 extends Level
 
-const IMAGE_TOTAL_PIXELS : int = 1679
-const CORRECT_WIDTH : int = 23
-
 var image_width : int = 1
 
 @onready var source_image : Image = load('res://data/image.png')
@@ -20,7 +17,9 @@ func _ready() -> void:
   update_display()
 
 func update_display() -> void:
-  var image_height : int = roundi(float(IMAGE_TOTAL_PIXELS) / float(image_width))
+  var source_size := source_image.get_size()
+  var pixel_count := source_size.x * source_size.y
+  var image_height : int = roundi(float(pixel_count) / float(image_width))
   lbl_size.text = '%dx%d' % [image_width, image_height]
 
   # Redraw the image with the given width and height. Just copy the pixels from
@@ -49,7 +48,7 @@ func _on_sld_width_value_changed(value: float) -> void:
   update_display()
 
 func _on_btn_confirm_pressed() -> void:
-  if image_width == CORRECT_WIDTH:
+  if image_width == source_image.get_width():
     LevelManager.switch_to_next_level_or_quit()
   else:
     print("That doesn't look quite right")
