81fb4f39cefcc0573abd62a0648eaf5b9eb59755
Matt Giuca <matt.giuca@gmail.com>
2026-04-19 15:21:42 +1000

    Waveform: mouse wheel to zoom in and out.

--- scenes/ui/waveform.gd
+++ scenes/ui/waveform.gd
@@ -2,6 +2,10 @@
 class_name Waveform
 extends Control
 
+const ZOOM_TICK_PERCENT : float = 1.1
+const MIN_DURATION : float = 0.005
+const MAX_DURATION : float = 5.0
+
 ## The audio to display.
 @export var audio_stream : AudioStreamWAV:
   set(value):
@@ -12,16 +16,16 @@ extends Control
 
 @export var start_time : float:
   set(value):
-    start_time = value
+    start_time = clampf(value, 0.0, audio_stream.get_length())
     queue_redraw()
 
 @export var end_time : float = 0.5:
   set(value):
-    end_time = value
+    end_time = clampf(value, 0.0, audio_stream.get_length())
     queue_redraw()
 
 func _ready() -> void:
-  pass
+  gui_input.connect(_on_gui_input)
 
 func _draw() -> void:
   if not audio_stream:
@@ -41,3 +45,23 @@ func _draw() -> void:
     points.append(Vector2(percent_time, percent_sample) * canvas_size)
   const LINE_WIDTH = 1.0
   draw_polyline(points, Color.LIGHT_BLUE, LINE_WIDTH, true)
+
+func _on_gui_input(event: InputEvent) -> void:
+  if event is InputEventMouseButton:
+    var mouse_event := event as InputEventMouseButton
+    if mouse_event.button_index == MOUSE_BUTTON_WHEEL_UP:
+      change_zoom(1, mouse_event.position)
+    elif mouse_event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+      change_zoom(-1, mouse_event.position)
+
+func change_zoom(dir: float, pos: Vector2) -> void:
+  var old_dur := end_time - start_time
+
+  var pos_x := pos.x
+  var pos_time := (pos.x / size.x) * old_dur + start_time
+
+  # Aim to keep pos_time the same before and after.
+  var new_dur := old_dur / ZOOM_TICK_PERCENT if dir > 0 else old_dur * ZOOM_TICK_PERCENT
+  new_dur = clampf(new_dur, MIN_DURATION, MAX_DURATION)
+  start_time = pos_time - (pos.x / size.x) * new_dur
+  end_time = start_time + new_dur
