63d338fe33d0a5c335f659488669d6d6d1d9f697
Matt Giuca <matt.giuca@gmail.com>
2026-04-20 01:40:26 +1000

    Added story text for endings.

--- scenes/levels/ending.gd
+++ scenes/levels/ending.gd
@@ -7,11 +7,18 @@ func _ready() -> void:
   hud.hide_help_button()
 
 const STORY_DIPLOMACY : String = """\
-DIPLOMACY ENDING
+We can learn much from the senders of this mysterious message, as they can from us.
+
+Envoys are being dispatched to the far-away planet. It will take many years, but some day \
+we hope there can be [color=light_green]peace[/color] between our two civilizations.
 """
 
 const STORY_CONFLICT : String = """\
-CONFLICT ENDING
+[color=indian_red][shake]Fools[/shake][/color]! They gave us their exact location, \
+and now we know there is a habitable, resource-rich world for the taking!
+
+Battle fleets are being dispatched to the far-away planet. It will take many years, but \
+some day, after [color=indian_red]glorious battle[/color], we will plant our flag on our new home world.
 """
 
 func _on_btn_diplomacy_pressed() -> void:
